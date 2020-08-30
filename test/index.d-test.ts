72ce038fcb8b54e03198a7573d3875be67f61ab8
Francesco Rivola <francesco.rivola@gmail.com>
2020-08-30 17:48:03 +0200

    extended index.d-test.ts

--- test/index.d-test.ts
+++ test/index.d-test.ts
@@ -1,18 +1,26 @@
 import createRateLimiter from '../index';
+import { expectAssignable } from 'tsd'
 
-const limiter = createRateLimiter({ concurrency: 1 });
+const options = { concurrency: 1};
+expectAssignable<{
+    concurrency: number;
+}>(options);
+
+const limiter = createRateLimiter(options);
+
+expectAssignable<<T>(func: (hold: (options: { holdMs: number, retry?: boolean}) => void) => Promise<T>) => Promise<T>>(limiter);
 
 type A = { a: number; };
 let retry = true;
 
 async function main() {
-    console.log(await limiter<A>(async hold => {
-        console.log(retry);
+    await limiter<A>(async hold => {
+        expectAssignable<(options: { holdMs: number, retry?: boolean}) => void>(hold);
         if (retry) {
             hold({ holdMs: 1, retry: true });
         }
         return { a: 1 };
-    }));
+    });
     process.exit(0);
 }
 main();
