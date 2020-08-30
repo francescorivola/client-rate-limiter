812e858a6892501ed50185918a917757c7113d04
Francesco Rivola <francesco.rivola@gmail.com>
2020-08-30 15:43:08 +0200

    improved typescript types

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 declare namespace ClientRateLimiter {
     export type CreateClientRateLimiterOptions = { concurrency: number };
-    export type HoldOptions = { holdMs: number, retry: boolean };
+    export type HoldOptions = { holdMs: number, retry: boolean = false };
     export type Hold = (options: HoldOptions) => void;
     export type ClientRateLimiterFunc = (hold: Hold) => Promise<any>;
     export type ClientRateLimiter = (func: ClientRateLimiterFunc) => Promise<any>;
-	export type CreateClientRateLimiter = (options: CreateClientRateLimiterOptions) => void;
+	export type CreateClientRateLimiter = (options: CreateClientRateLimiterOptions = { concurrency: 1 }) => void;
 }
 
 declare const createClientRateLimiter: clientRateLimiter.createClientRateLimiter;
